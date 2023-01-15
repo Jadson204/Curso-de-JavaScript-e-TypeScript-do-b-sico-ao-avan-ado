c85c8f83e26b7b22759e714221cdf4d91218bbf0
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-15 11:00:28 -0300

    19 - Tipos de dados Primitivos

---
+++ 02 JavaScript Basico/19-Dados-Primitivos/script.js
@@ -0,0 +1,8 @@
+const nome = 'Jadson'; // String
+const nome1 = "Jadson";
+const nome2 = `Jadson`;
+const num1 = 5; // Number
+const num2 = 5.68;
+let nomeAluno; // Undefined -> Não aponta para lugar nenhum na memória
+let sobrenomeAluno = null; // Nulo -> Não aponta para lugar nenhum na memória
+const aprovado = false; // Boolean = true, false (lógico)
