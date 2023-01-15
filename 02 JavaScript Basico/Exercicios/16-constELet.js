d13df19741cded43d2089b218de24b24e05c1da6
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-14 22:09:25 -0300

    16 - Exercícios - Const e let

---
+++ 02 JavaScript Basico/Exercicios/16-constELet.js
@@ -0,0 +1,7 @@
+const nome = 'Jadson Henrique de Lima';
+const idade = 30;
+const peso = 84;
+const alturaEmM = 1.80;
+const imc = peso / (alturaEmM * alturaEmM);
+
+console.log(nome, 'tem', idade, 'anos, pesa', peso, 'kg, tem', alturaEmM, 'de altura e seu IMC é de', imc);
