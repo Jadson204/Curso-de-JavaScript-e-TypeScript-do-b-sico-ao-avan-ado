3c4425633efdb356040af74f5035eefeb63967ed
Jadson204 <jadsonhenrique2004@gmail.com>
2023-01-26 22:34:19 -0300

    20 - Operadores aritméticos, de atribuição e incremento

---
+++ 02 JavaScript Basico/20-Operadores/script.js
@@ -0,0 +1,34 @@
+/**
+ * Aritméticos + - * /
+ */
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num2 % num1);
+console.log(num1 ** num2);
+
+let contador = 1;
+contador++;
+console.log(contador);
+
+let contador2 = 1;
+contador--;
+console.log(contador); 
+
+const passo = 2;
+let contador3 = 0;
+
+contador3 = contador3 + passo;
+console.log(contador3);
+contador3 = contador3 + passo;
+console.log(contador3);
+contador3 = contador3 + passo;
+console.log(contador3);
+
+const n1 = 10;
+const n2 = parseFloat('5.2');
+console.log(n1 + n2);
+console.log(typeof n2);
