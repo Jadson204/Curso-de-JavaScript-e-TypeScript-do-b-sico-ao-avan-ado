56c6e0d32e6716c74971b4b9552c1316ec5974b6
Jadson204 <jadsonhenrique2004@gmail.com>
2023-02-06 06:38:42 -0300

    52 - For Clássico - Estrutura de repetição

---
+++ 03 Javascript - Logica de Programacao/52-For/script.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 10; i++) {
+  console.log(`Linha ${i}`);
+}
+
+for(let i = 0; i <= 10; i++) {
+  const par = i % 2 === 0 ? 'par' : 'ímpar';
+  console.log(i, par);
+}
+
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for(let i = 0; i < frutas.length; i++) {
+  console.log(`Índice ${i} -`, frutas[i]);
+}
